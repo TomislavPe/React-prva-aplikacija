92fd1447992b3bdeb4d847506ce9758fa75d4076
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-08-31 20:25:29 +0200

    napravi zadatak

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import logo from './logo2.svg';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Moja prva aplikacija</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
